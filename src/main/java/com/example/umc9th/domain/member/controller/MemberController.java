445c9a850c6d769c26f03ffb865fe761fb1cedef
Ncs89 <shcksthf01@naver.com>
2025-11-10 15:02:59 +0900

    7주차 미션 완료

--- src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc9th/domain/member/controller/MemberController.java
@@ -5,19 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
 
 import com.example.umc9th.domain.member.entity.Member;
 import com.example.umc9th.domain.member.service.MemberService;
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
+
     private final MemberService memberService;
 
     @GetMapping("/{memberId}")
-    public ResponseEntity<Member> getMember(@PathVariable Long memberId) {
-        return ResponseEntity.ok(memberService.getMemberById(memberId));
+    public ApiResponse<Member> getMember(@PathVariable Long memberId) {
+        Member member = memberService.getMemberById(memberId);
+        return ApiResponse.onSuccess(GeneralSuccessCode.OK, member);
     }
 }
